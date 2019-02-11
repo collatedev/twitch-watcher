5bcdcb6ffffaee62a0ab2769ca6bc02b4c559a18
Evan Coulson <evan.m.coulson@gmail.com>
2019-02-10 23:56:17 -0800

    refactored user route

--- src/routes/UserRouter.ts
+++ src/routes/UserRouter.ts
@@ -1,6 +1,8 @@
 import Router from "./Router";
 import * as Express from "express";
 import UserLayer from "../layers/UserLayer";
+import StatusCodes from "./StatusCodes";
+import { Logger } from "../config/Winston";
 
 export default class UserRouter extends Router {
 	private userLayer : UserLayer;
@@ -17,20 +19,17 @@ export default class UserRouter extends Router {
 
 	public async getUserByID(request: Express.Request, response: Express.Response) {
 		let userID = parseFloat(request.params.userID);
-		if (this.isValidID(userID)) {
-			await this.sendUserData(response, userID);
-		} else {
-			this.sendError(response, `The Twitch User ID must be a positive integer value, instead received '${userID}'`, 400);
+		if (!this.isValidID(userID)) {
+			Logger.error("")
+			this.sendError(response, `The Twitch User ID must be a positive integer value, instead received '${userID}'`, StatusCodes.BadRequest);
 		}
-		
-	}
-
-	private async sendUserData(response: Express.Response, userID: number) {
 		try {
 			let user = await this.userLayer.getUserInfo(userID);
-			this.sendData(response, user, 200);
+			Logger.info(`Successfully got user: ${JSON.stringify(user)}`);
+			this.sendData(response, user, StatusCodes.OK);
 		} catch (error) {
-			this.sendError(response, `Failed to get user with id: ${userID}`, 404);
+			Logger.error(error);
+			this.sendError(response, `Failed to get user with id: ${userID}`, StatusCodes.NotFound);
 		}
 	}
 	
