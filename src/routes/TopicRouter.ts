3a9331f4e02bb54b34c40c71a6b0ca0ab3fcac22
Evan Coulson <evan.m.coulson@gmail.com>
2019-02-12 15:39:42 -0800

    preparing for hok testing

---
+++ src/routes/TopicRouter.ts
@@ -0,0 +1,26 @@
+import Router from "./Router"
+import { Request, Response } from "express"
+
+export default abstract class TopicRouter extends Router {
+    private topic : string;
+
+    constructor(topic: string) {
+        super(`/topic`);
+        this.topic = topic;
+        this.handleChallenge = this.handleChallenge.bind(this);
+        this.handleWebhookCall = this.handleWebhookCall.bind(this);
+    }
+
+    public setup() {
+        this.router.get(this.topic, this.handleChallenge);
+        this.router.post(this.topic, this.handleWebhookCall);
+    }
+
+    public async handleChallenge(request: Request, response: Response) {
+
+    }
+
+    public async handleWebhookCall(request: Request, response: Response) {
+
+    }
+}
---
+++ tests/routes/StreamRouter.spec.ts
@@ -0,0 +1,18 @@
+import SubscriptionRouter from "../../src/routes/SubscriptionRouter"
+import { use, expect } from 'chai';
+import 'mocha';
+import FakeUserModel from "../mocks/FakeUserModel";
+import { mockReq, mockRes } from 'sinon-express-mock'
+import * as sinonChai from 'sinon-chai'
+
+use(sinonChai);
+
+describe('Subscription Router', () => {
+    describe('handleChallenge', () => {
+
+    });
+
+    describe('handleWebhookCall', () => {
+        
+    })
+});
